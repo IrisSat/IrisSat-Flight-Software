59320c326d84f72f094c7a9ac4c1ae17e828a656
aminya <aminyahyaabadi74@gmail.com>
2020-11-23 21:56:47 -0600

    Paths

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -9,3 +9,18 @@ parser.add_argument("--debug", dest="is_debug", default=False, help="Enable debu
 # Parse arguments
 args = parser.parse_args()
 
+################################################################
+# Paths
+import os
+from os.path import abspath
+
+# Set cwd
+print("Changing current working directory to the repository root")
+fileDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(fileDir)
+
+is_windows = (os.name == 'nt' or (os.getenv('SYSTEMROOT') is not None and 'windows' in os.getenv('SYSTEMROOT').lower()) or (os.getenv('COMSPEC') is not None and 'windows' in os.getenv('COMSPEC').lower()))
+
+softconsole_path = "./iris-fsw-softconsole"
+libraries_path = "./iris-fsw-softconsole/Libraries"
+
